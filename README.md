09f9ce8c6e047dfafb7d4c8cd6551cd3f71b9d41
iclr2025-No9242 <iclr2025_No.9242@proton.me>
2024-09-30 22:07:15 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# aitqe
+# AITQE
+
+### Requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+### Usage
+
+The aitqe model can be downloaded from [here](https://huggingface.co/No9242/aitqe).
+
+```bash
+python inference.py --image_path ./test.png --caption "Some random text to the image like this is a test" --model_path ./path/to/aitqe --gpu_id 0
+```
+
+If you want to use aitqe to output only overall score (and possiblely the rewritten caption), you can use the following command:
+
+```bash
+python inference.py --image_path ./test.png --caption "Some random text to the image like this is a test" --model_path ./path/to/aitqe --gpu_id 0 --overall_only
+```
+
+You may need to download the language model used in the aitqe, and vision encoder used in the aitqe, 
+
+```bash
+https://huggingface.co/Qwen/Qwen2-7B
+https://huggingface.co/google/siglip-so400m-patch14-384
+```
+
+and specify the path to the language model and vision encoder in the config.json file in the aitqe model folder.
+
---
+++ inference.py
@@ -0,0 +1,89 @@
+import argparse
+import torch
+from modeling_mm import MMForCausalLM
+import json
+
+
+def move_tensors_to_cuda(data_dict, gpu_id):
+    if torch.cuda.is_available():
+        for key, value in data_dict.items():
+            if isinstance(value, torch.Tensor):
+                data_dict[key] = value.to(f'cuda:{gpu_id}')
+    else:
+        print("CUDA is not available.")
+    return data_dict
+
+def process_prompt(image_path: str, text: str):
+    image_dict = {
+        "path": image_path,
+        "source": "localpath",
+        "file_type": 'I'
+    }
+    question = f"Evaluate the caption: {text}\nif score is low, give a short description of the image.\nRespond using a dictionary structure."
+    return f"<img_start>{json.dumps(image_dict)}<img_end>\n{question}"
+
+
+def eval_single_node(model, tokenizer, image_path, text, overall_only=False, gpu_id=0, max_frame_number=4):
+    processor = model.bind_processor(tokenizer,
+                                     config=model.config,
+                                     max_frame_number=max_frame_number)
+    prompt = process_prompt(image_path, text)
+    proc_ret = processor(prompt)
+    proc_ret = move_tensors_to_cuda(proc_ret, gpu_id)
+    with torch.inference_mode():
+        if overall_only:
+            output_ids = model.generate(**proc_ret,
+                                    do_sample=False,
+                                    num_beams=1,
+                                    stop_strings=["<Overall>"],
+                                    tokenizer=tokenizer,
+                                    max_new_tokens=384,
+                                    use_cache=True)
+        else:
+            output_ids = model.generate(**proc_ret,
+                                    do_sample=False,
+                                    num_beams=1,
+                                    max_new_tokens=384,
+                                    use_cache=True)
+    outputs = tokenizer.batch_decode(
+        output_ids[:, proc_ret["input_ids"].shape[1]:],
+        skip_special_tokens=True)[0].strip()
+    return outputs
+
+
+def load_model(model_path, gpu_id):
+    model = MMForCausalLM.from_pretrained(model_path,
+                                            device_map=f'cuda:{gpu_id}',
+                                            torch_dtype=torch.bfloat16).eval()
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path,
+        padding_side="left",
+        truncation_side="left",
+        use_fast=False,
+        trust_remote_code=True,
+    )
+    return model, tokenizer
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpu_id", type=int, default=0)
+    parser.add_argument("--model_path", type=str, default="path/to/aitqe")
+    parser.add_argument("--image_path", type=str, default="./test.png")
+    parser.add_argument("--caption", type=str, default="Some random text to the image like this is a test")
+    parser.add_argument("--overall_only", action="store_true")
+    args = parser.parse_args()
+
+    model, tokenizer = load_model(args.model_path, args.gpu_id)
+    output = eval_single_node(model, tokenizer, args.image_path, args.caption, args.overall_only, args.gpu_id)
+    print("="*100)
+    print("[Input]")
+    print(f"Image:   {args.image_path}")
+    print(f"Caption: {args.caption}")
+    print("="*100)
+    print("[Output]")
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
